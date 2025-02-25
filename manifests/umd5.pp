7c60000420ff0643f82263c892c05b08a0235d9b
Enrico Vianello <enrico.vianello@cnaf.infn.it>
2025-02-25 16:28:40 +0100

    Updated umd5 urls

--- manifests/umd5.pp
+++ manifests/umd5.pp
@@ -14,11 +14,11 @@ class umd::umd5 (
   yumrepo { 'UMD-5':
     ensure   => present,
     descr    => 'UMD 5 main repository (Alma Linux 9)',
-    baseurl  => 'https://repository.egi.eu/repository/umd/5/al9/release/$basearch',
+    baseurl  => 'https://repository.egi.eu/software/umd/5/al9/release/$basearch',
     enabled  => 1,
     priority => 1,
     gpgcheck => 1,
-    gpgkey   => 'https://repository.egi.eu/repository/umd/5/UMD-5-RPM-PGP-KEY',
+    gpgkey   => 'https://repository.egi.eu/software/umd/5/UMD-5-RPM-PGP-KEY',
   }
   if $enable_contrib {
     $enable_contrib_num = 1
@@ -28,11 +28,11 @@ class umd::umd5 (
   yumrepo { 'UMD-5-contrib':
     ensure   => present,
     descr    => 'UMD 5 contrib repository (Alma Linux 9)',
-    baseurl  => 'https://repository.egi.eu/repository/umd/5/al9/contrib/$basearch',
+    baseurl  => 'https://repository.egi.eu/software/umd/5/al9/contrib/$basearch',
     enabled  => $enable_contrib_num,
     priority => 1,
     gpgcheck => 1,
-    gpgkey   => 'https://repository.egi.eu/repository/umd/5/UMD-5-RPM-PGP-KEY',
+    gpgkey   => 'https://repository.egi.eu/software/umd/5/UMD-5-RPM-PGP-KEY',
   }
   if $enable_testing {
     $enable_testing_num = 1
@@ -42,10 +42,10 @@ class umd::umd5 (
   yumrepo { 'UMD-5-testing':
     ensure   => present,
     descr    => 'UMD 5 testing repository (Alma Linux 9)',
-    baseurl  => 'https://repository.egi.eu/repository/umd/5/al9/testing/$basearch',
+    baseurl  => 'https://repository.egi.eu/software/umd/5/al9/testing/$basearch',
     enabled  => $enable_testing_num,
     priority => 1,
     gpgcheck => 1,
-    gpgkey   => 'https://repository.egi.eu/repository/umd/5/UMD-5-RPM-PGP-KEY',
+    gpgkey   => 'https://repository.egi.eu/software/umd/5/UMD-5-RPM-PGP-KEY',
   }
 }
